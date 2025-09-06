7ab27fd5e9975887c3527a98e071f6b498fa8904
AS1 <arseniykofanov@gmail.com>
2025-09-06 22:10:04 +0300

    Скелет для календаря

---
+++ calendar-api/main.py
@@ -0,0 +1,26 @@
+import asyncio
+import uvicorn
+from modules.api import app
+from modules.logs import calendar_logger
+
+async def api_start():
+    calendar_logger.info("Starting Calendar API...")
+
+    config = uvicorn.Config(app, 
+                            host="0.0.0.0", 
+                            port=8001, # 8001
+                            log_level="info",
+                            )
+    server = uvicorn.Server(config)
+    await server.serve()
+
+async def main():
+    tasks = [
+        asyncio.create_task(api_start())
+    ]
+
+    # Запускаем все таски параллельно
+    await asyncio.gather(*tasks)
+
+if __name__ == "__main__":
+    asyncio.run(main())
