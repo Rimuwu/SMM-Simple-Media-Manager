21361cd0c78ca53736e5fb16563ef2337969fd8c
AS1 <arseniykofanov@gmail.com>
2025-09-07 19:24:25 +0300

    ендпоинт для создания постов в вк

--- executors/routers/test.py
+++ executors/routers/test.py
@@ -1,10 +1,13 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Request
+from vk.main import VKExecutor
 from modules.executors_manager import manager
+from global_modules.limiter import limiter
 
 router = APIRouter(prefix="/test", tags=["test"])
 
-@router.get("/send-message/{executor_name}/{chat_id}")
-async def test(executor_name: str, chat_id: int):
+@router.post("/send-message/{executor_name}/{chat_id}")
+@limiter.limit("5/minute")
+async def test(request: Request, executor_name: str, chat_id: int):
     executor = manager.get(executor_name)
 
     if not executor:
@@ -14,3 +17,18 @@ async def test(executor_name: str, chat_id: int):
         chat_id, "Test message from send-message")
 
     return {"message": f"sended to {chat_id}"}
+
+@router.get("/create-post")
+@limiter.limit("5/minute")
+async def create_post(request: Request):
+    executor: VKExecutor = manager.get('vk_executor')
+
+    if not executor:
+        return {"error": "Executor not found"}
+
+    text = "Создание поста через endpoint /create-post"
+    attachments =  []
+
+    result = await executor.create_wall_post(text, attachments)
+
+    return result
