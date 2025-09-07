9dc2c324b2953fb5208fad31e07d385e56ea5af7
AS1 <arseniykofanov@gmail.com>
2025-09-07 17:05:45 +0300

    Выбор исполнителя

--- executors/routers/test.py
+++ executors/routers/test.py
@@ -3,13 +3,14 @@ from modules.executors_manager import manager
 
 router = APIRouter(prefix="/test", tags=["test"])
 
-@router.get("/send-message/{chat_id}")
-async def test(chat_id: int):
-    tg = manager.get("telegram_executor")
-    if not tg:
+@router.get("/send-message/{executor_name}/{chat_id}")
+async def test(executor_name: str, chat_id: int):
+    executor = manager.get(executor_name)
+
+    if not executor:
         return {"error": "Telegram executor not found"}
 
-    await tg.send_message(
+    await executor.send_message(
         chat_id, "Test message from send-message")
 
     return {"message": f"sended to {chat_id}"}
