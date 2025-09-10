554f2f43766b35d45aa68011a3204131280f62a9
AS1 <arseniykofanov@gmail.com>
2025-09-10 13:22:02 +0300

    get -> post с данными

--- executors/routers/test.py
+++ executors/routers/test.py
@@ -2,23 +2,31 @@ from fastapi import APIRouter, Request
 from vk.main import VKExecutor
 from modules.executors_manager import manager
 from global_modules.limiter import limiter
+from pydantic import BaseModel
 
 router = APIRouter(prefix="/test", tags=["test"])
 
-@router.post("/send-message/{executor_name}/{chat_id}")
+class MessageData(BaseModel):
+    executor_name: str
+    chat_id: int
+    text: str = "Test message from /test/send-message"
+
+@router.post("/send-message")
 @limiter.limit("5/minute")
-async def test(request: Request, executor_name: str, chat_id: int):
-    executor = manager.get(executor_name)
+async def test(request: Request, message_data: MessageData):
+    executor = manager.get(message_data.executor_name)
+    chat_id = message_data.chat_id
 
     if not executor:
         return {"error": "Telegram executor not found"}
 
     await executor.send_message(
-        chat_id, "Test message from send-message")
+        chat_id, message_data.text
+    )
 
     return {"message": f"sended to {chat_id}"}
 
-@router.get("/create-post")
+@router.post("/create-post")
 @limiter.limit("5/minute")
 async def create_post(request: Request):
     executor: VKExecutor = manager.get('vk_executor')
