1603e25edd1b99a8442a82dc24f25f3d74f30083
AS1 <arseniykofanov@gmail.com>
2025-09-10 13:19:58 +0300

    Чёт не хотелись модели с связями удаляться, поэтому это новый способ удаления

--- brain-api/database/core.py
+++ brain-api/database/core.py
@@ -3,14 +3,17 @@
 
 from config import Settings
 from database.connection import Base, engine
+from sqlalchemy import text
+import models
 
 async def create_tables():
     """Удалить все таблицы и пересоздать их заново."""
 
     engine.echo = False
     async with engine.begin() as conn:
-        # Удаляем все таблицы
-        await conn.run_sync(Base.metadata.drop_all)
+        # Удаляем все таблицы с CASCADE для обхода зависимостей
+        await conn.execute(text("DROP SCHEMA public CASCADE"))
+        await conn.execute(text("CREATE SCHEMA public"))
         # Создаём заново
         await conn.run_sync(Base.metadata.create_all)
     engine.echo = getattr(Settings, "debug", False)
