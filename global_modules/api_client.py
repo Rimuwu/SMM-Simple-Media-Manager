46904cdd039b0225cf09b163cb055f752fd568db
AS1 <arseniykofanov@gmail.com>
2025-09-10 13:18:47 +0300

    Возврат вторым аргументом код ответа

--- global_modules/api_client.py
+++ global_modules/api_client.py
@@ -5,12 +5,13 @@ class APIClient:
     def __init__(self, base_url: str):
         self.base_url = base_url
 
-    async def get(self, endpoint: str, params: dict = None):
+    async def get(self, endpoint: str, 
+                  params: dict = None):
         async with aiohttp.ClientSession() as session:
             async with session.get(f"{self.base_url}{endpoint}", params=params) as response:
-                return await response.json()
+                return await response.json(), response.status
 
     async def post(self, endpoint: str, data: dict = None):
         async with aiohttp.ClientSession() as session:
             async with session.post(f"{self.base_url}{endpoint}", json=data) as response:
-                return await response.json()
+                return await response.json(), response.status
