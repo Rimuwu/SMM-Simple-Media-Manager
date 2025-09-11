b9fedbf6e50932da5cf50ae1c4a79e4265c26fde
AS1 <arseniykofanov@gmail.com>
2025-09-11 18:00:36 +0300

    Занчение по умолчанию  для tasker_id = None

--- brain-api/models/User.py
+++ brain-api/models/User.py
@@ -11,7 +11,7 @@ class User(Base):
 
     user_id: Mapped[uuidPK]
     telegram_id: Mapped[int] = mapped_column(Integer, unique=True, nullable=False)
-    tasker_id: Mapped[str] = mapped_column(String, nullable=True)
+    tasker_id: Mapped[str] = mapped_column(String, nullable=True, default=None)
     role: Mapped[UserRole] = mapped_column(nullable=False, default=UserRole.copywriter)
 
     # Связи
