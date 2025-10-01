19fcce3aa3870173f20a6d62ca34e27b80e64062
AS1 <arseniykofanov@gmail.com>
2025-10-01 13:42:17 +0300

    -

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from kaiten_client import KaitenClient
+from global_modules.kaiten_client import KaitenClient
 from os import getenv
 from dotenv import load_dotenv
 import asyncio
@@ -8,21 +8,10 @@ load_dotenv()
 TOKEN = getenv('KAITEN_TOKEN')
 DOMAIN = getenv('KAITEN_DOMAIN')
 
-async def main(): 
-    
-    async with KaitenClient(TOKEN, DOMAIN) as client:
-
-        # space = await client.get_spaces()
-        # board = await client.get_boards(space[0].id)
-        # column = await client.get_columns(board[0].id)
-        # card = await client.create_card('werwer', 
-        #                                 column_id=column[0].id,
-        #                                 board_id=board[0].id)
-        
-        card = await client.get_card(55371436)
-        print(await card.get_members())
-        
-        # await card.add_member(908324)
+def main(): 
+    pass
 
 
-asyncio.run(main())
+# asyncio.run(main())
+main()
+
