17f434ea263c7079ba6833fb8c2baddd77a114df
AS1 <arseniykofanov@gmail.com>
2025-10-02 21:48:18 +0300

    -

--- brain-api/models/__init__.py
+++ brain-api/models/__init__.py
@@ -1,12 +1,12 @@
 # Импортируем все модели в правильном порядке для SQLAlchemy
 from .User import User, UserRole
 from .Card import Card, CardStatus  
-from .Message import Message, MessageType
+# from .Message import Message, MessageType
 from .Automation import Automation, AutomationTypes, Preset
 
 __all__ = [
     "User", "UserRole",
     "Card", "CardStatus", 
-    "Message", "MessageType",
+    # "Message", "MessageType",
     "Automation", "AutomationTypes", "Preset"
 ]
