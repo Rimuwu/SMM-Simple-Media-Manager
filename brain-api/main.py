3390cb56d6b28a5e1dbbf3875dbaf94adfc29bf9
AS1 <arseniykofanov@gmail.com>
2025-10-16 17:59:15 +0300

    -

--- brain-api/main.py
+++ brain-api/main.py
@@ -6,7 +6,7 @@ from database.core import create_superuser, create_tables
 from global_modules.limiter import limiter
 from global_modules.middlewares.logs_mid import RequestLoggingMiddleware
 
-from routers.db_health import router as db_health_router
+from routers.standart import router as standart_router
 # from routers.user import router as user_router
 from routers.card import router as card_router
 from routers.ai import router as ai_router
@@ -16,6 +16,38 @@ from global_modules.api_configurate import get_fastapi_app
 from modules.logs import brain_logger
 from modules.kaiten import kaiten
 from modules.config_kaiten import sync_kaiten_settings
+
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    # Startup
+    brain_logger.info("API is starting up...")
+    brain_logger.info("Creating missing tables on startup...")
+    await create_tables()
+    await create_superuser()
+    
+    # await kaiten_check()
+    
+    # await sync_kaiten_settings()
+    
+    yield
+
+    # Shutdown
+
+app = get_fastapi_app(
+    title="API",
+    version="1.0.0",
+    description="Brain API",
+    debug=False,
+    lifespan=lifespan,
+    limiter=True,
+    middlewares=[],
+    routers=[
+        standart_router, card_router, ai_router
+    ],
+    api_logger=brain_logger
+)
+app.add_middleware(RequestLoggingMiddleware, logger=brain_logger)
 
 async def kaiten_check():
 
@@ -46,38 +78,23 @@ async def kaiten_check():
         # pp = await client.get_custom_properties()
         # pprint(pp)
 
-@asynccontextmanager
-async def lifespan(app: FastAPI):
-    # Startup
-    brain_logger.info("API is starting up...")
-    brain_logger.info("Creating missing tables on startup...")
-    await create_tables()
-    await create_superuser()
-    
-    await kaiten_check()
-    
-    await sync_kaiten_settings()
-    
-    yield
+# async def test_db():
+#     from models.User import User
+#     from models.Card import Card
+#     from models.Automation import Automation, Preset
+#     from uuid import uuid4
 
-    # Shutdown
-
-app = get_fastapi_app(
-    title="API",
-    version="1.0.0",
-    description="Brain API",
-    debug=False,
-    lifespan=lifespan,
-    limiter=True,
-    middlewares=[],
-    routers=[
-        db_health_router, card_router, ai_router
-    ],
-    api_logger=brain_logger
-)
-app.add_middleware(RequestLoggingMiddleware, logger=brain_logger)
-
-@app.get("/")
-@limiter.limit("2/second")
-async def root(request: Request):
-    return {"message": f"{app.description} is running! v{app.version}"}
+#     # Создание пользователя
+    
+#     user, status = await User.first_or_create(
+#         user_id=uuid4(),
+#         telegram_id=123456789,
+#     )
+#     pprint(user.to_dict())
+    
+#     await user.update(telegram_id=987654321)
+#     pprint(user.to_dict())
+    
+#     user = await User.get_by_id(user.user_id)
+#     if user:
+#         pprint(user.to_dict())
