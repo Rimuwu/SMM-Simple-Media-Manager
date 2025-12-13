d6714b083df86167bb04be7eb06225cd0be405d2
AS1 <arseniykofanov@gmail.com>
2025-12-14 01:54:21 +0300

    -

--- executors/tg/scenes/edit/tags_page.py
+++ executors/tg/scenes/edit/tags_page.py
@@ -7,17 +7,17 @@ class TagsSetterPage(TagsSelectorPage):
     __page_name__ = 'tags-setter'
     __scene_key__ = 'tags_list'
     update_to_db = True
-    
+
     async def update_to_database(self, tags_list: list) -> bool:
         """Обновляем теги в карточке при выходе со страницы"""
         task_id = self.scene.data['scene'].get('task_id')
         if not task_id:
             return False
-        
+
         # Обновляем карточку
         success = await brain_client.update_card(
             card_id=task_id,
             tags=tags_list
         )
-        
+
         return success is not None
